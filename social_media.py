6c41322b64b7a4e5e108499dd288334ffbd1f3ec
Nkurikiyimana Aimable <aimablenkurikiyimana1@gmail.com>
2025-01-29 15:03:49 +0200

    tests

--- social_media.py
+++ social_media.py
@@ -1,11 +1,9 @@
 # creating simple database for simple social network backend
+
 from sqlalchemy import create_engine, ForeignKey, String, Integer, Column
-
-# declarative base: which we will use to create models or classes for our database
 from sqlalchemy.orm import declarative_base
-
-# importing session maker
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy import func
 import uuid
 
 Base = declarative_base()
@@ -25,13 +23,18 @@ class Users(Base):
     first_name = Column("first_name", String, nullable=False)
     last_name = Column("last_name", String, nullable=False)
     profile_name = Column("profile_name", String, nullable=False)
-    email = Column("email", String, nullable=True)
+    email = Column(String, nullable=True)
 
     def __init__(self, first_name, last_name, profile_name, email):
         self.first_name = first_name
         self.last_name = last_name
         self.profile_name = profile_name
         self.email = email
+
+    def __str__(self):
+        return (
+            f"{self.first_name} + {self.last_name} + {self.profile_name} + {self.email}"
+        )
 
 
 # Post class: for creating a post record
@@ -103,29 +106,19 @@ Base.metadata.create_all(bind=engine)
 # creating a session
 Session = sessionmaker(bind=engine)
 
-# add a user record to the database
-# add_user("Jeanluc", "Nkurikiye", "@jeanlu", "jeanluc@gmail.com", Session)
+# add_user("Didier", "Muhire", "@didos", "didier@gmail.com", Session)
 
-# add a post record to the database
-# user_id = "6f77faba-1c02-44dc-9935-d76626d13f9c"
-# add_post(user_id, "How have studying at KSS been for me, not what you expect!@..", Session)
+# Users: 6
+# Likes: 9
+# Posts: 9
 
-# all_posts = (
-#     Session()
-#     .query(Posts)
-#     .filter(Posts.userId == "6f77faba-1c02-44dc-9935-d76626d13f9c")
-#     .all()
-# )
-# posts_ = [post.post_content for post in all_posts]
-# print(posts_)
-# for post in all_posts:
-#     print(post.post_content)
+# try querying some data..
+session = Session()
+objs = [Users, Likes, Posts]
+count = []
+for obj in objs:
+    count += session.query(obj)
 
-# like post..
-user_id = "6f77faba-1c02-44dc-9935-d76626d13f9c"
-post_id = "4a56d213-4cc7-45ee-88d1-d92ed4cf0206"
-like_post(user_id, post_id, Session)
+print(len(count))
 
-#trying to retrieve a like id
-likes  = Session().query(Likes).filter(Likes.post_id == "4a56d213-4cc7-45ee-88d1-d92ed4cf0206").count()
-print(f"this post (4a56d213-4cc7-45ee-88d1-d92ed4cf0206) has {likes} likes")
+
